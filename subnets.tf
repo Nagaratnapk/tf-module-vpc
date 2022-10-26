b6927b7689b1ccca42d77827a6d1f7965199a51b
Nagaratna Karetimmannanavar <nagaratnakaretimmannanavar@Nagaratnas-MacBook-Pro.local>
2022-10-25 21:33:01 -0700

    update

--- subnets.tf
+++ subnets.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public" {
   count               = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id              = aws_vpc.main.id
   cidr_block          = element(var.PUBLIC_SUBNET_CIDR, count.index)
@@ -6,6 +6,17 @@ resource "aws_subnet" "main" {
 
   tags = {
     Name = "${var.ENV}-pub-${element(var.AZ, count.index)}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count               = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id              = aws_vpc.main.id
+  cidr_block          = element(var.PRIVATE_SUBNET_CIDR, count.index)
+  availability_zone   = element(var.AZ, count.index)
+
+  tags = {
+    Name = "${var.ENV}-prv-${element(var.AZ, count.index)}"
   }
 }
 
